a520e9e90ae648e6f8a8480b63ef0cb488fff092
nekokuroneko <snsd.ssset.ykr.1031@gmail.com>
2024-07-10 21:19:31 +0900

    migrate_Kondo

---
+++ database/migrations/2024_07_06_051712_create_curriculums_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculums', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('thumbnail')->nullable();
+            $table->longText('description')->nullable();
+            $table->mediumText('video_url')->nullable();
+            $table->tinyInteger('alway_delivery_flg');
+            $table->integer('grade_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculums');
+    }
+};
