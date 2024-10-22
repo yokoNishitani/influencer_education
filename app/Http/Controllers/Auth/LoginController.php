6e406288f04521848cf27d3fffdc6bd9132eefa0
mei0528 <mei.ume1215@gmail.com>
2024-10-22 23:23:24 +0900

    Your commit message

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\User\Auth;
+
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\User\DeliveryController;
 use App\Http\Controllers\YourController;
+use App\Http\Controllers\Auth\User\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
